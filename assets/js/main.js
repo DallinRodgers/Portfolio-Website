b59a46b9ed3ab3cde9894b72ffa75ecc3b08a12b
DallinRodgers <rodgers.dallin@gmail.com>
2020-02-19 14:55:29 -0700

    Update main.js
    removed nav scroll affect

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+const nav = document.querySelector(".nav-links");
+
 const navSlide = () => {
   const burger = document.querySelector(".burger");
   const navLinks = document.querySelectorAll(".nav-links li");
@@ -23,46 +25,45 @@ const navSlide = () => {
 
 navSlide();
 
-const nav = document.querySelector(".nav-links");
-let scrollpos = window.scrollY;
-const header = document.querySelector("nav");
-const logo = document.querySelector(".logo");
-const links = document.querySelectorAll(".nav-link");
-const burgerLines = document.querySelectorAll(".line");
-const header_height = header.offsetHeight;
+// let scrollpos = window.scrollY;
+// const header = document.querySelector("nav");
+// const logo = document.querySelector(".logo");
+// const links = document.querySelectorAll(".nav-link");
+// const burgerLines = document.querySelectorAll(".line");
+// const header_height = header.offsetHeight;
 
-function add_class_on_scroll(el, cl) {
-  const element = el;
-  const classToAdd = cl;
-  element.classList.add(classToAdd);
-}
-function remove_class_on_scroll(el, cl) {
-  const element = el;
-  const classToAdd = cl;
-  element.classList.remove(classToAdd);
-}
+// function add_class_on_scroll(el, cl) {
+//   const element = el;
+//   const classToAdd = cl;
+//   element.classList.add(classToAdd);
+// }
+// function remove_class_on_scroll(el, cl) {
+//   const element = el;
+//   const classToAdd = cl;
+//   element.classList.remove(classToAdd);
+// }
 
-window.addEventListener("scroll", function() {
-  scrollpos = window.scrollY;
-  if (scrollpos >= header_height) {
-    add_class_on_scroll(header, "fade-in");
-    add_class_on_scroll(logo, "text-fade-in");
-    add_class_on_scroll(nav, "nav-links-dark");
-    for (let i = 0; i < links.length; i++) {
-      add_class_on_scroll(links[i], "text-fade-in");
-    }
-    for (let i = 0; i < burgerLines.length; i++) {
-      add_class_on_scroll(burgerLines[i], "background-fade-in");
-    }
-  } else {
-    remove_class_on_scroll(header, "fade-in");
-    remove_class_on_scroll(logo, "text-fade-in");
-    remove_class_on_scroll(nav, "nav-links-dark");
-    for (let i = 0; i < links.length; i++) {
-      remove_class_on_scroll(links[i], "text-fade-in");
-    }
-    for (let i = 0; i < burgerLines.length; i++) {
-      remove_class_on_scroll(burgerLines[i], "background-fade-in");
-    }
-  }
-});
+// window.addEventListener("scroll", function() {
+//   scrollpos = window.scrollY;
+//   if (scrollpos >= header_height) {
+//     add_class_on_scroll(header, "fade-in");
+//     add_class_on_scroll(logo, "text-fade-in");
+//     add_class_on_scroll(nav, "nav-links-dark");
+//     for (let i = 0; i < links.length; i++) {
+//       add_class_on_scroll(links[i], "text-fade-in");
+//     }
+//     for (let i = 0; i < burgerLines.length; i++) {
+//       add_class_on_scroll(burgerLines[i], "background-fade-in");
+//     }
+//   } else {
+//     remove_class_on_scroll(header, "fade-in");
+//     remove_class_on_scroll(logo, "text-fade-in");
+//     remove_class_on_scroll(nav, "nav-links-dark");
+//     for (let i = 0; i < links.length; i++) {
+//       remove_class_on_scroll(links[i], "text-fade-in");
+//     }
+//     for (let i = 0; i < burgerLines.length; i++) {
+//       remove_class_on_scroll(burgerLines[i], "background-fade-in");
+//     }
+//   }
+// });
